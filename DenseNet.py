acb59ea3b339024aea3a1413e732dbfbf1ae5471
UESBTC <527994785@qq.com>
2019-12-05 20:59:27 +0800

    完成，未调试

--- DenseNet.py
+++ DenseNet.py
@@ -1,12 +1,19 @@
 import tensorflow as tf
 import tflearn
-from implementation import *
+from implement import *
 class_num=100
 dropout_rate=0.2
+image_size=32
+channel_num=3
+growth_rate=12
+init_learning_rate=1e-4
+epsilon=1e-4
+total_epoch=300
+iteration=782
+test_iteration=157
+batch_size=64
 
-
-
-def conv_layer(x,filter_num,kernel_size,stride=1,scope):
+def conv_layer(x,filter_num,kernel_size,stride=1,scope=None):
 	with tf.name_scope(scope):
 		x=tf.layers.conv2d(inputs=x,use_bias=False,filters=filter_num,kernel_size=kernel_size,strides=stride,padding='SAME')
 	return x
@@ -27,6 +34,17 @@ def linear(x):
 	return tf.layers.dense(inputs=x,units=class_num,name='linear')
 def global_average_pooling(x,stride=1):
 	return tflearn.layers.conv.global_avg_pool(x,name='global_average_pooling')
+	# return None
+def evaluate(sess,test_data,test_labels):
+	test_feed_dict={
+		x:data,
+		y_:test_labels,
+		training_flag:False
+	}
+	_, test_loss,test_acc=sess.run(accuracy,feed_dict=test_feed_dict)
+	return test_loss,test_acc
+
+
 class DenseNet:
 	def __init__(self,data,k,training):
 		self.data=data
@@ -81,6 +99,67 @@ class DenseNet:
 		# x=self.trainsition_block(x,scope='trains_2')
 		# x=self.dense_block(x,num_in_block=32,scope='dense_3')
 		return x
+def train():
+	train_data,train_labels,test_data,test_labels=prepare_data()
+	train_data,test_data=color_preprocess(train_data,test_data)
+	x=tf.placeholder(tf.float32,shape=[None,image_size,image_size,channel_num])
+	y_=tf.placeholder(tf.float32,shape=[None,class_num])
+	training_flag=tf.placeholder(tf.bool)
+	learning_rate=tf.placeholder(tf.float32,name='learning_rate')
+
+	logits=DenseNet(data=train_data,k=growth_rate,training=training_flag).model
+	cost=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=logits))
+
+	train=tf.train.AdamOptimizer(learning_rate=learning_rate,epsilon=epsilon).minimize(cost)
+	correct_prediction=tf.equal(tf.argmax(logits,1),tf.argmax(y_,1))
+	accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+	saver=tf.train.Saver()
+
+	with tf.Session() as sess:
+		ckpt=tf.train.get_checkpoint_state('./model')
+		if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+			saver.restore(sess,ckpt.model_checkpoint_path)
+		else:
+			tf.global_veriables_initializer().run()
+		cur_learning_rate=init_learning_rate
+		for epoch in range(1,total_epoch+1):
+			if epoch==(0.5*total_epoch) or epoch==(0.75*total_epoch):
+				epoch/=10
+			pre_ind=0
+			train_loss=0.0
+			train_acc=0.0
+			for step in (1,iteration+1):
+				if pre_ind+batch_size<50000:
+					batch_x=train_data[pre_ind:pre_ind+batch_size]
+					batch_y=train_labels[pre_ind:pre_ind+batch_size]
+				else:
+					batch_x=train_data[pre_ind:]
+					batch_y=train_labels[pre_ind:]
+				batch_x=data_augmentation(batch_x)
+
+				train_feed_dict={
+					x:batch_x,
+					y_:batch_y,
+					training_flag:True,
+					learning_rate:cur_learning_rate
+				}
+				_, batch_loss=sess.run([train,cost],feed_dict=train_feed_dict)
+				batch_acc=accuracy.eval(feed_dict=train_feed_dict)
+				train_loss+=batch_loss
+				train_acc+=batch_acc
+				pre_ind+=batch_size
+
+				if step==iteration:
+					train_loss/=iteration
+					train_acc/=iteration
+					test_loss,test_acc=evaluate(sess,test_data,test_labels)
+					print('epoch:%d/%d, train_loss=%.4f, train_acc=%.4f, test_loss:%.4f, test_acc:%.4f\n'%(epoch,train_loss,train_acc,test_loss,test_acc))
+		saver.save(sess=sess,save_path='./model/dense.ckpt')
+
+
+
+
+
 
 
 
